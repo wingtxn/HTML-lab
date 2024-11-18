58acbe283cb7f95e31e6fce9c6badf9648c300ac
wingtxn <wingtxn@gmail.com>
2024-11-17 19:33:13 -0700

    Feat: Add Greeting to Home Page

--- javascript directory/file.js
+++ javascript directory/file.js
@@ -4,5 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const fYear = document.getElementById("footerYear");
     const d = new Date();
     fYear.textContent = d.getFullYear();
-});
 
+    //-- Greeting ------
+    const greeting = document.getElementById("greetings");
+    const currentHour = new Date().getHours();
+
+    let greetingText;
+    let greetingClass;
+
+    if (currentHour < 8) {
+        greetingText = "Rise & Shine!";
+        greetingClass = "early-morning";
+    } else if (currentHour < 12) {
+        greetingText = "Good Morning!";
+        greetingClass = "morning";
+    } else if (currentHour < 17) {
+        greetingText = "Good Afternoon!";
+        greetingClass = "afternoon";
+    } else {
+        greetingText = "Good Evening!";
+        greetingClass = "evening";
+    }
+
+    greeting.textContent = greetingText;
+    greeting.classList.add(greetingClass);
+
+});
